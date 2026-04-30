c455279bc325fa1c73782e4032829a2c10bf6a0f
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-04-30 19:27:06 +0530

    create components and style them

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
+import Contact from "./components/contact/Contact"
+import Portfolio from "./components/portfolio/Portfolio"
+import Hero from "./components/hero/Hero"
+import Services from "./components/services/Services"
+
 const App = () => {
   return (
-    <div className=''>Hello World</div>
+    <div className='container'>
+
+    <section id="#home">
+    <Hero/>
+    </section>
+
+    <section id="#services">
+    <Services/>
+    </section>
+
+    <section id="#portfolio">
+    <Portfolio/>
+    </section>
+
+    <section id="#contact">
+    <Contact/>
+    </section>
+
+    </div>
   )
 }
 
