1229b8c0eddfe55ab0c5e45fdd6f7ca973b3edca
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-01 21:05:37 +0530

    bg setup

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,30 +4,30 @@ import Speech from "./Speech"
 const Hero = () => {
   return (
     <div className='hero'>
-      
+
       {/* LEFT SECTION */}
       <div className="hSection left">
         <h1 className="hTitle">
           Hey There,
           <br />
-          <span>I'm Ayan</span>
+          <span>I'm Ayan!</span>
         </h1>
 
-      <div className="hIntro">
+        <div className="hIntro">
           <h2 className="hSubtitle">Web Developer</h2>
 
-        <p className="hDesc">
-          I build responsive and dynamic web applications using modern technologies 
-          like React, JavaScript and MySQL.
-        </p>
-      </div>
-        
+          <p className="hDesc">
+            I build responsive and dynamic web applications using modern technologies
+            like React, JavaScript and MySQL.
+          </p>
+        </div>
+
 
         {/* TECH STACK (replaces awards) */}
         <div className="awards">
           <h2>Tech Stack</h2>
           <p>Technologies I work with</p>
-          
+
           <div className="awardList">
             <img src="./html.png" alt="HTML" />
             <img src="./css.png" alt="CSS" />
@@ -39,12 +39,12 @@ const Hero = () => {
         {/* SCROLL INDICATOR */}
         <a href="#services" className="scroll">
           <svg width="40" height="70" viewBox="0 0 24 40" fill="none">
-            <rect 
-              x="2" y="2" 
-              width="20" height="36" 
+            <rect
+              x="2" y="2"
+              width="20" height="36"
               rx="10"
-              stroke="white" 
-              strokeWidth="2" 
+              stroke="white"
+              strokeWidth="2"
             />
 
             <circle cx="12" cy="10" r="2" fill="white">
@@ -101,7 +101,7 @@ const Hero = () => {
           <a href="/#contact" className="contactList">
             <div className="contactButton">
               <svg viewBox="0 0 200 200" width="150" height="150">
-                
+
                 <circle cx="100" cy="100" r="90" fill="pink" />
 
                 <path
@@ -122,20 +122,28 @@ const Hero = () => {
                   </textPath>
                 </text>
 
-                
+
 
               </svg>
-               <div className="arrow">
-                  <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" stroke="black" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
-  <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"/>
-</svg>
-                </div>
+              <div className="arrow">
+                <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="none" stroke="black" class="bi bi-arrow-up-right" viewBox="0 0 16 16">
+                  <path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z" />
+                </svg>
+              </div>
             </div>
-           
+
           </a>
         </div>
 
       </div>
+<div className="bg">
+  {/* 3d */}
+  <div className="hImg">
+    <img src="/hero.png" alt="" />
+  </div>
+</div>
+
+
     </div>
   )
 }
