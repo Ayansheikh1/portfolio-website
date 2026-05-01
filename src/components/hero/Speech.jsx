807471116fdd13c7bff2d9d6e6340166a1b6999f
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-05-01 18:01:04 +0530

    react type animation implement

--- src/components/hero/Speech.jsx
+++ src/components/hero/Speech.jsx
@@ -1,12 +1,30 @@
+import { TypeAnimation } from 'react-type-animation';
+
 const Speech = () => {
-  return (
-    <div className="bubbleContainer" >
-        <div className="bubble">
-            Text
+    return (
+        <div className="bubbleContainer" >
+            <div className="bubble">
+                <TypeAnimation
+                    sequence={[
+                        1000,
+                        "I build scalable web apps",
+                        1500,
+                        "Frontend + Backend Solutions",
+                        1500,
+                        "Clean UI • Fast APIs",
+                        1500,
+                        "Open to projects & internships",
+                        1500,
+                    ]}
+                    wrapper="span"
+                    speed={40}
+                    deletionSpeed={60}
+                    repeat={Infinity}
+                />
+            </div>
+            <img src="/man.png" alt="" />
         </div>
-        <img src="/man.png" alt="" />
-    </div>
-  )
+    )
 }
 
 export default Speech
