0d324711767714e9760055f982b6e6006b3b8bd6
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-06-27 00:12:48 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,25 +1,59 @@
 import "./contact.css"
+import  { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
+import { span } from "motion/react-client";
+
 
 const Contact = () => {
+  const ref = useRef();
+
+  const[success,setSuccess] = useState(false);
+
+  const[error,setError] = useState(false);
+
+  const sendEmail = (e)=>{
+    e.preventDefaukt();
+
+     emailjs
+      .sendForm(import.meta.env.VITE_SERVICE_ID,
+         import.meta.env.VITE_TEMPLATE_ID, 
+         e.target, {
+        publicKey: import.meta.env.VITE_PUBLIC_KEY,
+      })
+      .then(
+        () => {
+          setSuccess(true);
+          setError(false);
+        },
+        (error) => {
+          console.log(error)
+         setError(true);
+         setSuccess(false);
+        },
+      );
+  }
+
   return (
     <div className='contact'>
       <div className="cSection">
-        <form>
+        <form ref={ref} onSubmit={sendEmail} >
           <h1 className="cTitle">Let's keep in touch</h1>
           <div className="formItem">
             <label>Name</label>
-            <input type="text" placeholder="John doe"/>
+            <input type="text" name="user_username" placeholder="John doe"/>
           </div>
           <div className="formItem">
             <label>Email</label>
-            <input type="email" placeholder="John@gmail.com"/>
+            <input type="email" name="user_email" placeholder="John@gmail.com"/>
           </div>
           <div className="formItem">
             <label>Message</label>
-            <textarea rows={10} placeholder="write your message..."></textarea>
+            <textarea rows={10} name="user_message" placeholder="write your message..."></textarea>
           </div>
 
           <button className="formButton">Send</button>
+          {success && <span>Your message has been sent!</span>}
+          {error && <span>Something went wrong!</span>}
           
 
         </form>
