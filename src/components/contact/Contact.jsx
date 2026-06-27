4046b015bf23960544e9ab41896c32669b7eb04a
Ayan Sheikh <ayansheikh0698@gmail.com>
2026-06-27 19:37:05 +0530

    Add new assets and implement contact form functionality
    - Introduced new JavaScript file for intersection observer functionality.
    - Added multiple image assets including awards, services, and social media icons.
    - Created an HTML file for the portfolio structure.
    - Updated the Contact component to improve code readability and functionality, including email sending logic and form handling.

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import "./contact.css"
-import  { useRef, useState } from 'react';
+import { useRef, useState } from 'react';
 import emailjs from '@emailjs/browser';
 
 import { motion, useInView } from "motion/react";
@@ -11,80 +11,88 @@ const Contact = () => {
   const ref = useRef();
   const form = useRef();
 
-  const[success,setSuccess] = useState(false);
+  const [success, setSuccess] = useState(false);
 
-  const[error,setError] = useState(false);
+  const [error, setError] = useState(false);
 
 
   const listVariants = {
-    initial:{
-      x:100,
-      opacity:0
+    initial: {
+      x: 100,
+      opacity: 0
 
     },
-    animate:{
-      x:0,
-      opacity:1,
-      transition:{
+    animate: {
+      x: 0,
+      opacity: 1,
+      transition: {
         duration: 0.5,
-        staggerChildren:0.2
+        staggerChildren: 0.2
 
       }
 
     }
   }
 
-  const isInView = useInView(ref,{margin:'-200px'})
+  const isInView = useInView(ref, { margin: '-200px' })
 
-  const sendEmail = (e)=>{
+  const sendEmail = (e) => {
     e.preventDefault();
 
-     emailjs
+   
+
+    emailjs
       .sendForm(import.meta.env.VITE_SERVICE_ID,
-         import.meta.env.VITE_TEMPLATE_ID, 
-         form.current, {
+        import.meta.env.VITE_TEMPLATE_ID,
+        form.current, {
         publicKey: import.meta.env.VITE_PUBLIC_KEY,
       })
       .then(
         () => {
           setSuccess(true);
           setError(false);
+
+          form.current.reset();
         },
         (error) => {
-          console.log(error)
-         setError(true);
-         setSuccess(false);
+          console.log(error);
+          setError(true);
+          setSuccess(false);
         },
       );
   }
 
   return (
-    <div className='contact' ref={form}  onSubmit={sendEmail}>
-      <div className="cSection">
-        <motion.form variants={listVariants} animate={isInView ?"animate":"initial"} ref={ref}  >
+    <div className='contact'   >
+      <div className="cSection " ref={ref}>
+        <motion.form
+          variants={listVariants}
+          animate={isInView ? "animate" : "initial"}
+          ref={form}
+          onSubmit={sendEmail}  >
           <motion.h1 variants={listVariants} className="cTitle">Let's keep in touch</motion.h1>
-          <motion.div variants={listVariants}  className="formItem">
+          <motion.div variants={listVariants} className="formItem">
             <label>Name</label>
-            <input type="text" name="user_username" placeholder="John doe"/>
+            <input type="text" name="user_username" placeholder="John doe" />
           </motion.div>
-          <motion.div variants={listVariants}  className="formItem">
+          <motion.div variants={listVariants} className="formItem">
             <label>Email</label>
-            <input type="email" name="user_email" placeholder="John@gmail.com"/>
+            <input type="email" name="user_email" placeholder="John@gmail.com" />
           </motion.div>
-          <motion.div variants={listVariants}  className="formItem">
+          <motion.div variants={listVariants} className="formItem">
             <label>Message</label>
             <textarea rows={10} name="user_message" placeholder="write your message..."></textarea>
           </motion.div>
 
-          <motion.button variants={listVariants}  className="formButton">Send</motion.button>
+          <motion.button variants={listVariants} className="formButton">Send</motion.button>
           {success && <span>Your message has been sent!</span>}
           {error && <span>Something went wrong!</span>}
-          
+
 
         </motion.form>
       </div>
       <div className="cSection">
-        <ContactSvg/>
+        <ContactSvg />
       </div>
     </div>
   )
